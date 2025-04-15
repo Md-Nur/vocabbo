53051188c18a02f19592bc6c327c5d6b7dd367c8
Muhammad Nur <md.nurealamsiddiquee2004@gmail.com>
2025-04-16 04:27:23 +0600

    home page previous route store

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,10 +1,18 @@
+"use client";
 import Comparison from "@/components/Home/Comparison";
 import Faq from "@/components/Home/Faq";
 import FeatureCards from "@/components/Home/FeatureCards";
 import Hero from "@/components/Home/Hero";
 import WhyVocabbo from "@/components/Home/WhyVocabbo";
+import { storePreviousRoute } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
 
 const Home = () => {
+  const pathname = usePathname();
+  useEffect(() => {
+    storePreviousRoute(pathname);
+  }, []);
   return (
     <section className="w-full h-full">
       <Hero />
